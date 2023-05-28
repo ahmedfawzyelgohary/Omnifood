2e4b11714f969a9baf2da4b44b0a2dc232ceb1d1
elgohary <ahmedfawyelgohary@yahoo.com>
2023-05-28 21:26:06 +0300

    make nav open and close with js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,41 @@
 ///////////////////////////////////////////////////////////
-// make mobile navigation work
-const btnnavEL = document.querySelector(".btn-mobile-nav");
-const headerEL = document.querySelector(".header");
-btnnavEL.addEventListener("click", function () {
-  headerEL.classList.toggle("nav-open");
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
